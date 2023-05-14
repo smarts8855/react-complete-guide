134105e464229e96217e51e5a9e5b79ebba1b53a
smarts8855 <85696168+smarts8855@users.noreply.github.com>
2023-05-14 20:20:11 +0100

    react-complete-guide

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesChart from "./ExpensesChart";
+import './Expenses.css';
+import Card from "../UI/Card";
+
+
+const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear)
+    };
+
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    
+    return(
+        <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses}/>
+              
+        </Card>
+    )
+    
+}
+
+export default Expenses;
